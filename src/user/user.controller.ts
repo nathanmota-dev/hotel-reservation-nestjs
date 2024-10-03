afdb594af90a30ce69848aa086156040026e43bf
Nathan <nathansmota@gmail.com>
2024-10-02 22:40:35 -0300

    :sparkles: feat: create user crud

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { Prisma, User as UserModel } from '@prisma/client';
 import { UserService } from './user.service';
 
@@ -13,6 +13,27 @@ export class UserController {
         @Body() userData: Prisma.UserCreateInput,
     ): Promise<UserModel> {
         return this.userService.createUser(userData);
+    }
+
+    @Get(':id')
+    async getUserById(@Param('id') id: string): Promise<UserModel> {
+        return this.userService.user({ id: Number(id) });
+    }
+
+    @Put()
+    async updateUser(
+        @Body() userData: Prisma.UserUpdateInput,
+        @Param('id') id: string,
+    ): Promise<UserModel> {
+        return this.userService.updateUser({
+            where: { id: Number(id) },
+            data: userData,
+        });
+    }
+
+    @Delete(':id')
+    async deleteUser(@Param('id') id: string): Promise<UserModel> {
+        return this.userService.deleteUser({ id: Number(id) });
     }
 }
 
