3e4c963268ba8140c699d232fd03c7ce8b1e2c7b
Nathan Mota <nathansmota@gmail.com>
2026-04-30 15:03:28 -0300

    :wrench: chore: add Prisma config

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -1,8 +1,17 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaLibSql } from '@prisma/adapter-libsql';
+
+const adapter = new PrismaLibSql({
+    url: 'file:./prisma/hotel.db',
+});
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+    constructor() {
+        super({ adapter });
+    }
+
     async onModuleInit() {
         await this.$connect();
     }
